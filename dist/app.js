8e51d2586dcd0d652958635fe03e6827648fa28e
robbridges <robbridges@users.noreply.github.com>
2021-05-05 19:26:29 -0700

    rendered form successfully

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,13 @@ class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
+        const node = document.importNode(this.templateElement.content, true);
+        this.element = node.firstElementChild;
+        this.attachNode();
+    }
+    attachNode() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
+const projectInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
+  element: HTMLFormElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
     
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const node = document.importNode(this.templateElement.content, true);
+    this.element = node.firstElementChild as HTMLFormElement;
+    this.attachNode();
+  }
+  private attachNode() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
+
+const projectInput = new ProjectInput();
