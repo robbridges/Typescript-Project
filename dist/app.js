8178d5910007e6075883c19f535554a74e71fffb
robbridges <robbridges@users.noreply.github.com>
2021-05-07 18:04:22 -0700

    Setting up element node and event listener

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,10 @@ class ProjectInput {
         this.hostElement = document.getElementById('app');
         const node = document.importNode(this.templateElement.content, true);
         this.element = node.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInput = this.element.querySelector('#title');
+        this.descriptionInput = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
         this.attachNode();
     }
     attachNode() {
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInput: HTMLInputElement;
+  descriptionInput: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,7 +14,22 @@ class ProjectInput {
 
     const node = document.importNode(this.templateElement.content, true);
     this.element = node.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInput = this.element.querySelector('#title')as HTMLInputElement;
+    this.descriptionInput = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attachNode();
+  }
+
+  private submitHandler(event: Event) {
+    
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
   private attachNode() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
